92b3a51ea4e508265d38fff8b4bb00ab4393e137
Cristian Perez <cperezlecaros@gmail.com>
2025-03-12 17:29:09 -0700

    Install mongoose and config and create app.config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
+    MongooseModule.forRoot(EnvConfiguration().mongodb),
+    TasksModule,
+  ],
 })
 export class AppModule {}
