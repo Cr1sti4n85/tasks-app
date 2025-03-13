4784f76ea3e11f6e13d0e4b2b4f98d56bebd75cd
Cristian Perez <cperezlecaros@gmail.com>
2025-03-12 17:57:51 -0700

    Create tasks service methods

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({ timestamps: true })
-class Task {
+export class Task {
   @Prop({
     type: String,
     required: true,
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from '../schemas/task.schema';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectModel(Task.name) private readonly taskModel: Model<Task>,
+  ) {}
+
+  async findAll() {
+    await this.taskModel.find();
+  }
+
+  async create(createTask: any) {
+    const newTask = new this.taskModel(createTask);
+    await newTask.save();
+    return newTask;
+  }
+
+  async findOne(id: string) {
+    await this.taskModel.findById(id);
+  }
+
+  async update(id: string, updateTask: any) {
+    await this.taskModel.findByIdAndUpdate(id, updateTask, { new: true });
+  }
+
+  async remove(id: string) {
+    await this.taskModel.findByIdAndDelete(id);
+  }
+}
