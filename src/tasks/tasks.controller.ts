231dbea6b1abec98403c177b6d49900549059109
Cristian Perez <cperezlecaros@gmail.com>
2025-03-14 16:16:30 -0700

    Create task controller and update main for validation pipe

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly taskService: TasksService) {}
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get()
+  findOne(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() task: CreateTaskDto) {
+    return this.taskService.create(task);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() task: CreateTaskDto) {}
+}
